b81848c96e378b682f643b43e330ab52813ed2bc
Solano de Zuasnabar <sdezuasnabar@gmail.com>
2024-04-02 17:38:11 -0300

    spelling errors corrected

--- src/app/prices/page.jsx
+++ src/app/prices/page.jsx
@@ -38,35 +38,35 @@ const ContactPage = () => {
         },
         {
             title: 'Precio mensual',
-            info: 'La duración es de 30 días de corrido desde el día que lo abonas. Por ejemplo, si pagaste un 15 de mes, tu cuota vence el 15 del mes siguiente.',
+            info: 'La duración es de 30 días de corrido desde el día que lo abonas. Por ejemplo, si pagaste un 15 de mes, tu cuota mensual vence el 15 del mes siguiente.',
             price: '$21.400',
             discount: '$20.300',
             infoDiscount: 'Descuento del % pagando en efectivo.'
         },
         {
             title: 'Plan Personalizado ',
-            info: 'El plan personalizado, solo es accesible si tenés un acuerdo con alguno de nuestros entrenadores para entrenar exclusivamente con él. Pagas menos en tu cuota mensual del gimnasio pero pagas a tu entrenador el precio que acuerden entre ustedes. Solo es aplicable si el entrenador trabaja para Cúsipde Gym.',
+            info: 'El plan personalizado, solo es accesible si tenés un acuerdo con alguno de nuestros entrenadores para entrenar exclusivamente con él. Pagas menos en tu cuota mensual del gimnasio pero pagas también a tu entrenador el precio que acuerden entre ustedes. Solo es aplicable si el entrenador trabaja para Cúspide Gym.',
             price: '$17.100',
             discount: '$16.200',
             infoDiscount: 'Descuento del % pagando en efectivo.'
         },
         {
             title: 'Precio trimestral',
-            info: 'La duración es de tres meses de corrido desde el día que lo abonas. Por ejemplo, si pagaste un 15 marzo, tu cuota vence el 15 de junio del mismo año.',
+            info: 'La duración es de tres meses de corrido desde el día que lo abonas. Por ejemplo, si pagaste un 15 marzo, tu cuota trimestral vence el 15 de junio del mismo año.',
             price: '$54.100',
             discount: '$51.400',
             infoDiscount: 'Descuento del % pagando en efectivo.'
         },
         {
             title: 'Precio semestral',
-            info: 'La duración es de seis meses de corrido desde el día que lo abonas. Por ejemplo, si pagaste un 15 de agosto, tu cuota vence el 15 de febrero del año siguiente.',
+            info: 'La duración es de seis meses de corrido desde el día que lo abonas. Por ejemplo, si pagaste un 15 de agosto, tu cuota semestral vence el 15 de febrero del año siguiente.',
             price: '$103.000',
             discount: '$97.600',
             infoDiscount: 'Descuento del % pagando en efectivo.'
         },
         {
             title: 'Promoción anual',
-            info: 'La duración es de un año de corrido desde el día que lo abonas. Por ejemplo, si pagaste un 15 de marzo, tu cuota vence el 15 de marzo del año siguiente.',
+            info: 'La duración es de un año de corrido desde el día que lo abonas. Por ejemplo, si pagaste un 15 de marzo, tu cuota anual vence el 15 de marzo del año siguiente.',
             price: '$192.000',
             discount: '$182.400',
             infoDiscount: 'Descuento del % pagando en efectivo.'
